74cc2f674b19b57a4d1bf6758453d5c64411aca8
caio-fm <caio.moreira@ccc.ufcg.edu.br>
2022-02-20 20:46:21 -0300

    teste com jest

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/react';
+
+import React from "react";
+import { render as renderD, unmountComponentAtNode} from "react-dom";
+import { act } from "react-dom/test-utils"; 
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/backend/i);
   expect(linkElement).toBeInTheDocument();
 });
